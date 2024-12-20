02f89c95a3b0dc6089cd07dbdc19c599134d2b60
Akash <akashpaswan.iitkgp@gmail.com>
2024-12-21 03:45:12 +0530

    covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -11,15 +11,17 @@ class chatbook:
                             2. Press 2 to signin
                             3. Press 3 to write a post
                             4. Press 4 to message a friend
-                            5. Press any other key to exit""")
+                            5. Press any other key to exit
+                            
+                            ->""")
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input == "4":
-            pass
+            self.sendmsg()
         else:
             exit()
     def signup(self):
@@ -45,4 +47,21 @@ class chatbook:
         print("\n")
         self.menu()
         
-obj = chatbook()
+    def my_post(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here ->")
+            print(f"Folowing content has been posted -> {txt}")
+        else:
+            print("You need to signin first to post something")
+        print("\n")
+        self.menu()
+    def sendmsg(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here ->")
+            frnd = input("Whom to send the msg?")
+            print(f"Your message has been sent to {frnd}")
+        else:
+            print("You need to sign in first to send message")
+        print("\n")
+        self.menu()
+user1 = chatbook()
--- rough.py
+++ rough.py
@@ -4,3 +4,5 @@ my_int = 155
 # print(type(my_int))
 my_str = my_str.capitalize()
 print(my_str)
+from  oops_proj import chatbook
+user1 = chatbook()
